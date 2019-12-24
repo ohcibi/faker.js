b2a224dd491e7d998471af32a90d28452319fff1
Tobias Witt <tobias.witt@hhu.de>
2019-12-24 16:03:06 +0100

    Convert phone

---
+++ src/phone.ts
@@ -0,0 +1,48 @@
+/**
+ *
+ * @module phone
+ */
+
+import { replaceSymbolWithNumber } from 'helpers';
+import { arrayElement } from 'random';
+
+declare const faker: { definitions: any };
+
+/**
+ * phoneFormats
+ *
+ * @method faker.phone.phoneFormats
+ */
+export function phoneFormats(): string {
+  return arrayElement(faker.definitions.phone_number.formats);
+}
+
+/**
+ * phoneNumber
+ *
+ * @method faker.phone.phoneNumber
+ * @param {string} format
+ * @memberOf faker.phone
+ */
+export function phoneNumber(format?: string): string {
+  format = format || phoneFormats();
+  return replaceSymbolWithNumber(format);
+}
+
+// FIXME: this is strange passing in an array index.
+/**
+ * phoneNumberFormat
+ *
+ * @method faker.phone.phoneFormatsArrayIndex
+ * @param phoneFormatsArrayIndex
+ * @memberOf faker.phone
+ */
+export function phoneNumberFormat(phoneFormatsArrayIndex = 0): string {
+  return replaceSymbolWithNumber(faker.definitions.phone_number.formats[phoneFormatsArrayIndex]);
+}
+
+export default {
+  phoneFormats,
+  phoneNumber,
+  phoneNumberFormat
+};
