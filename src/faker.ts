c4649e878d83d79f07f9048b69bd0ac7b84a417f
Tobias Witt <tobias.witt@hhu.de>
2019-12-25 16:51:13 +0100

    Create faker main module

---
+++ src/faker.ts
@@ -0,0 +1,43 @@
+import address from 'address';
+import commerce from 'commerce';
+import company from 'company';
+import database from 'database';
+import date from 'date';
+import fake from 'fake';
+import finance from 'finance';
+import git from 'git';
+import hacker from 'hacker';
+import helpers from 'helpers';
+import iban from 'iban';
+import image from 'image';
+import internet from 'internet';
+import lorem from 'lorem';
+import name from 'name';
+import phone from 'phone';
+import random from 'random';
+import system from 'system';
+import unique from 'unique';
+import vehicle from 'vehicle';
+
+export default {
+  address,
+  commerce,
+  company,
+  database,
+  date,
+  fake,
+  finance,
+  git,
+  hacker,
+  helpers,
+  iban,
+  image,
+  internet,
+  lorem,
+  name,
+  phone,
+  random,
+  system,
+  unique,
+  vehicle
+};
