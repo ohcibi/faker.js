708a7a0c0866c119d29e33c6c96981118f27caaa
Tobias Witt <tobias.witt@hhu.de>
2020-01-15 12:28:28 +0100

    f definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -30,7 +30,10 @@ export function deleteLocale(code: keyof Registry): void {
   delete registry[code];
 }
 
-export function getDefinition(key: string, defaultValue?: string): string {
+export function getDefinition(
+  key: string,
+  defaultValue?: string
+): string | number | string[] | number[] {
   return key.split('.').reduce((ret: Locale | Definition | string, subKey) => {
     if (typeof ret === 'string') {
       return ret;
@@ -48,4 +51,20 @@ export function getDefinition(key: string, defaultValue?: string): string {
 
     return ret[subKey];
   }, registry[currentLocale]) as string;
+}
+
+export function getString(key: string, defaultValue?: string): string {
+  return getDefinition(key, defaultValue) as string;
+}
+
+export function getNumber(key: string, defaultValue?: string): number {
+  return getDefinition(key, defaultValue) as number;
+}
+
+export function getStringArray(key: string, defaultValue?: string): string[] {
+  return getDefinition(key, defaultValue) as string[];
+}
+
+export function getNumberArray(key: string, defaultValue?: string): number[] {
+  return getDefinition(key, defaultValue) as number[];
 }
